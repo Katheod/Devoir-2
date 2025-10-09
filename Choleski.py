67b42d6fbf9f87bcd10f3acc255076a29a2e0857
Mathis Boivin <mathis.boivin.2@ulaval.ca>
2025-10-09 08:41:06 -0400

    Implement Choleski decomposition and validation functions

---
+++ Choleski.py
@@ -0,0 +1,53 @@
+import numpy as np
+def validerCholeski(matrix):
+    symetrie = True
+    det_positif = False
+    n = len(matrix)
+    print(n)
+
+    for i in range(n):
+        for j in range(n):
+            if matrix[i][j] != matrix[j][i]:
+                symetrie = False
+                break
+
+    if np.linalg.det(matrix) > 0:
+        det_positif = True
+
+    return (symetrie and det_positif)
+
+
+def Choleski(matrix):
+    assert validerCholeski(matrix), "La matrice n'est pas symétrique définie positive"
+
+    res = np.zeros_like(matrix)
+    
+    l11 = np.sqrt(matrix[0][0])
+    res[0][0] = l11
+    n = len(matrix)
+
+    for i in range(1, n):
+        li1 = matrix[i][0]/ l11
+        res[i][0] = li1
+    
+    for k in range(1, n):
+        sum1 = 0
+        for j in range(k):
+            sum1 += res[k][j]**2
+        lkk = np.sqrt(matrix[k][k] - sum1)
+        res[k][k] = lkk
+
+        for i in range(k+1, n):
+            sum2 = 0
+            for p in range(k):
+                sum2 += res[i][j]*res[k][j]
+            lik = (matrix[i][k] - sum2)/lkk
+            res[i][k] = lik
+    
+    return res
+
+matrix = np.array([[4, 1, 2],
+                   [1, 2, 0],
+                   [2, 0, 3]])
+
+print(Choleski(matrix))  # True
