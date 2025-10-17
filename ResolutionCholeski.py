48ec0310f7db5e73fc4e495d32f094fbefccb325
etvac <etvac5@ulaval.ca>
2025-10-17 18:17:58 -0400

    Add inverse calculation for matrix A using Choleski

--- ResolutionCholeski.py
+++ ResolutionCholeski.py
@@ -20,3 +20,19 @@ A = np.array([[10, 7, 8, 7],
 b = np.array([32, 23, 33, 31])
 
 (ResolutionCholeski(A, b))
+
+
+
+#Inverse de A
+
+e1 = np.matrix.transpose(np.array([1,0,0,0]))
+e2 = np.matrix.transpose(np.array([0,1,0,0]))
+e3 = np.matrix.transpose(np.array([0,0,1,0]))
+e4 = np.matrix.transpose(np.array([0,0,0,1]))
+
+c1 = ResolutionCholeski(A,e1)
+c2 = ResolutionCholeski(A,e2)
+c3 = ResolutionCholeski(A,e3)
+c4 = ResolutionCholeski(A,e4)
+
+A1 = np.array([[c1],[c2],[c3],[c4]])
